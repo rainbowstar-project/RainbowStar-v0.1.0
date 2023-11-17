91ce0a93625bd7cc800d37bc924a91778a686aca
TulioBrant <tbrantsg@hotmail.com>
2023-11-17 20:33:00 -0300

    feat: fog of war

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -6,9 +6,12 @@ public class ParallaxBackground : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [Range(0f, 1f)]
-    [SerializeField] private float distance;
+    [SerializeField] private float movementFactor;
     [SerializeField] private SpriteRenderer spriteRef;
     private float xOffset, yOffset;
+
+    //if the player is moving into the background pivot the movementFactor needs to be adjusted.
+    [SerializeField] private float minDistanceFix;
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +24,10 @@ public class ParallaxBackground : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 newPos = target.position * distance;
-        Vector3 fixedPos = new Vector3(newPos.x, newPos.y, 10f);
+
+        Vector3 newPos = new Vector3(1f, -0.6f) * movementFactor;
+
+        Vector3 fixedPos = new Vector3(transform.position.x + newPos.x, transform.position.y + newPos.y, 10f);
         transform.position = fixedPos;
 
         if(Mathf.Abs(target.transform.position.x - transform.position.x) > xOffset)
