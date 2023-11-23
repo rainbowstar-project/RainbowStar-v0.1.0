22c9a1f0d8838812ec1ef01c1508292f6b40261b
TulioBrant <tbrantsg@hotmail.com>
2023-11-23 11:03:35 -0300

    feat: lose condition

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,11 +1,38 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
+    //gameOver index
+    [SerializeField] private Sprite whiteHole;
+    [SerializeField] private SpriteRenderer sr;
+    private bool canEnter;
+
+    private int gameOverLevel = 8;
+
+    private void Start()
+    {
+        canEnter = false;
+    }
+
+    public void UpdatePortal()
+    {
+        sr.sprite = whiteHole;
+        canEnter = true;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player")) {
-            LevelManager.instance.NextLevel();
+
+            if (canEnter)
+            {
+                LevelManager.instance.NextLevel();
+            }
+            else
+            {
+                SceneManager.LoadScene(gameOverLevel);
+            }
         }
     }
 }
